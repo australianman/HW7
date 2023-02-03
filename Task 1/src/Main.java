f4b77a606be9bbba7d59ea11d4ffd104edfd5358
australianman <animalwillingdick@gmail.com>
2023-02-03 18:38:41 -0300

    done

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,9 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int toadEyesCount = 0;          //Жабьи глаза
-        int ghoulTearsCount = 0;        //Слезы вурдалака
-        int ravenBonesCount = 0;        //Кости ворона
-        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        int i = 2;
+        while (i <= 100) {
+            System.out.println("Уволен айтишник с id:" +i);
+            i += 2;
+        }
+
+    }
+}
+
+
+
+
+//    public static void main(String[] args) {
+//for(int id = 2; id < 100; id++) {
+//    if(id % 2 == 0) {
+//        System.out.println("Уволен айтишник с id:" + id);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int toadEyesCount = 0;          //Жабьи глаза
+//        int ghoulTearsCount = 0;        //Слезы вурдалака
+//        int ravenBonesCount = 0;        //Кости ворона
+//        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+//        boolean isCanCook = false;
+//
+//
+//        System.out.println("Жабьи лапы:");
+//        toadEyesCount = new Scanner(System.in).nextInt();
+//        System.out.println("Слезы вурдалака:");
+//        ghoulTearsCount = new Scanner(System.in).nextInt();
+//        System.out.println("Кости ворона:");
+//        ravenBonesCount = new Scanner(System.in).nextInt();
+//        System.out.println("Пельмени:");
+//        dumplingsCount = new Scanner(System.in).nextInt();
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
 
@@ -13,6 +68,27 @@ public class Main {
         // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
+//        if ( toadEyesCount >= 3 && ravenBonesCount >= 1){
+//            System.out.println("Вы получили Эликсир зоркости");
+//            isCanCook = true;
+//
+//        }
+//        if ( ravenBonesCount >= 2 && dumplingsCount >= 4){
+//
+//            System.out.println("Вы получили эликсир стойкости");
+//            isCanCook = true;
+//        }
+//        if ( ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2){
+//            System.out.println("Вы получили элексир скрытности");
+//            isCanCook = true;
+//        }
+//        if ( ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 5 && ravenBonesCount >= 3){
+//            System.out.println("Вы получили запретный эликсир");
+//            isCanCook = true;
+//
+//        } if(isCanCook == false) {
+//            System.out.println("Миша все хуйня, давай по новой");
+
+
+
         //todo дописать логику программы сюда
-    }
-}
